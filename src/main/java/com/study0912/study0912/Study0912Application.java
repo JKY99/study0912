36dc001ea48add753694ec3376cee7702233b47d
장관영 <jkypch@adcapsule.co.kr>
2023-09-12 11:28:28 +0900

    study spring security 20230912

---
+++ src/main/java/com/study0912/study0912/Study0912Application.java
@@ -0,0 +1,13 @@
+package com.study0912.study0912;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Study0912Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Study0912Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/study0912/study0912/Study0912ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study0912.study0912;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Study0912ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
