19eace950fa7f2e8c43f7af7706f9c2de9465ada
장관영 <jkypch@adcapsule.co.kr>
2023-09-12 17:55:42 +0900

    SecurityConfig 설정 추가

--- src/main/java/com/study0912/study0912/SecurityConfig.java
+++ src/main/java/com/study0912/study0912/SecurityConfig.java
@@ -1,22 +1,34 @@
 package com.study0912.study0912;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.UserDetailsManagerConfigurer.UserDetailsBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     
-
-    public SecurityFilterChain filterChain(HttpSecurity http, final AuthenticationConfiguration authenticationConfiguration) throws Exception{
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
+            //SecurityContext를 명시적으로 저장하게 한다.
+            .securityContext(securityContext -> securityContext
+                .requireExplicitSave(true))
             .csrf(c->c.disable())
             .cors(c->c.disable())
+            .authorizeHttpRequests(authorize -> authorize
+                .anyRequest().authenticated())
             .formLogin(config->config
+                .loginPage("/login/")   
                 .permitAll())
             ;
-
 
         return http.build();
     }
